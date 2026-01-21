66f6a956c9d99fa133d513c01dc433da28f59fd5
GabRodPul <103140108+GabRodPul@users.noreply.github.com>
2026-01-21 17:49:08 +0000

    Several fixes + Cargo.toml info

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
-use core::time;
-use std::{error::Error, fs, time::Instant};
+use std::fs;
+use fantoccini::Client;
 use itertools::Itertools;
 use select::{
     document::Document,
-    predicate::{Attr, Name}
+    predicate::{Class, Name}
 };
 
-use scraper::{Html, Selector};
-use select::predicate::Class;
-use tokio::main;
-
-use clap::{Arg, Parser};
+use clap::{Parser};
 
 /// Arguments
 #[derive(Parser, Debug)]
@@ -24,37 +20,65 @@ struct Args {
     #[arg(short, long)]
     page_count: usize,
 
+    /// Chromedriver/Geckodriver port
     #[arg(short, long)]
-    chromedriver_port: u16
+    driver_port: u16,
+
+    /// Output file name
+    #[arg(short, long, default_value_t = String::from("result.csv"))]
+    output_file: String,
+
+    /// Saves the file when an element isn't found
+    #[arg(short, long, default_value_t = true)]
+    save_on_not_found: bool
+}
+
+// Fn's
+const CSV_HEADER: &str = "Title,Year,Directors,Rating10";
+fn save_csv(data: Vec<String>, out_str: &str) {
+    if let Some(d) = data.last() && d != CSV_HEADER {
+        println!("Saving a total of {} movies", data.len())
+    } else {
+        println!("Data empty when saving .csv - file will be written anyway.");
+    }
+
+    let mut content = String::with_capacity(10000);
+    for d in data {
+        content.push_str(&d);
+        content.push('\n');
+    }
+
+    fs::write(
+        std::path::Path::new(out_str),
+        content
+    ).unwrap();
 }
 
 #[tokio::main]
 async fn main() {
-    scrape().await
+    let args = Args::parse();
+    let driver_url: &str = &format!("http://localhost:{}", args.driver_port);
+    use fantoccini::{ClientBuilder};
+    let c = ClientBuilder::native().connect(driver_url).await.unwrap();
+    
+    scrape(args, &c).await;
+    c.close().await.unwrap();
 }
 
-async fn scrape() {
-    // let args = Args::parse();
-    let args = Args::parse();
+const STRUCTURE_CHANGE: &str = "FilmAffinity's HTML structure has changed and this repository needs updating";
+async fn scrape(args: Args, c: &Client) {
     let base_url: &str = &format!("https://www.filmaffinity.com/en/userratings.php?user_id={}&orderby=4", args.user_id);
-    let chromedriver_url: &str = &format!("http://localhost:{}", args.chromedriver_port);
     let mut data = Vec::<String>::with_capacity(50 * args.page_count);
-    data.push("Title,Year,Directors,Rating10".to_owned());
+    data.push(CSV_HEADER.to_owned());
 
     for p in 1..=args.page_count {
         let url = format!("{}&p={}&chv=list", base_url, p);
-        println!("{}", url);
-
+        println!("Processing URL: {}", url);
+        
         // FilmAffinity needs JS to do it's thing in order to display content
         // Plain HTML requests won't work
-        use fantoccini::{ClientBuilder};
-        let c = ClientBuilder::native().connect(chromedriver_url).await.unwrap();
         c.goto(&url).await.unwrap();
-        // c.maximize_window().await.unwrap();
-        // c.execute("document.getElementById('disagree-btn').click()", vec![]).await.unwrap();
-        // println!("{}", c.title().await.unwrap());
-
-        // let text: String = match reqwest::get(format!("{}&p={}&chv=list", BASE_URL, p)).await {
+        
         let text: String = match c.source().await {
             Err(e) => {
                 println!("{}", e);
@@ -62,23 +86,34 @@ async fn scrape() {
             },
             Ok(r) => r
         };
-        // println!("{}", text);
+
         let d = Document::from(&text[..]);
+        if let Some(_) = text.find("<h1>Not Found</h1>") {
+            println!("[ERROR] User with id {} not found", args.user_id);
+            return;
+        }
         drop(text);
 
         let movies = d.find(Class("mb-4")).skip(2).collect::<Vec<_>>();
+        if movies.is_empty() {
+            println!("[ERROR] {}", STRUCTURE_CHANGE);
+            return;
+        }
+
         for m in movies {
             let title = match
                 m.find(Class("mc-title")).collect::<Vec<_>>().first()
             {
                 None => {
-                    println!("[ERROR] Title: '<div>' not found.");
+                    println!("[ERROR] Title: '<div>' not found. {}", STRUCTURE_CHANGE);
+                    if args.save_on_not_found { save_csv(data, &args.output_file); }
                     return;
-                }
+                }   
                 Some(div) =>
                     match div.find(Class("d-md-none")).collect::<Vec<_>>().first() {
                         None => {
-                            println!("[ERROR] Title: '<a>' not found.");
+                            println!("[ERROR] Title: '<a>' not found. {}", STRUCTURE_CHANGE);
+                            if args.save_on_not_found { save_csv(data, &args.output_file); }
                             return;
                         },
                         Some(a) => a.text(),
@@ -89,7 +124,8 @@ async fn scrape() {
                 m.find(Class("mc-year")).collect::<Vec<_>>().first()
             {
                 None => {
-                    println!("[ERROR] Year: '<span>' not found.");
+                    println!("[ERROR] Year: '<span>' not found. {}", STRUCTURE_CHANGE);
+                    if args.save_on_not_found { save_csv(data, &args.output_file); }
                     return;
                 }
                 Some(span) => span.text(),
@@ -99,7 +135,8 @@ async fn scrape() {
                 m.find(Class("credits")).collect::<Vec<_>>().first()
             {
                 None => {
-                    println!("[ERROR] Year: '<span>' not found.");
+                    println!("[ERROR] Director: '<span>' not found. {}", STRUCTURE_CHANGE);
+                    if args.save_on_not_found { save_csv(data, &args.output_file); }
                     return;
                 }
                 Some(div) => div.find(Name("a")).map(|a| a.text()).collect::<Vec<_>>()
@@ -109,40 +146,26 @@ async fn scrape() {
                 m.find(Class("fa-user-rat-box")).collect::<Vec<_>>().first()
             {
                 None => {
-                    println!("[ERROR] Year: '<span>' not found.");
+                    println!("[ERROR] Rating: '<span>' not found. {}", STRUCTURE_CHANGE);
+                    if args.save_on_not_found { save_csv(data, &args.output_file); }
                     return;
                 }
                 Some(div) => match div.text().trim().parse::<i32>() {
-                    Err(e) => {
-                        // println!("{} - {}", e, title);
-                        // panic!("FUCK")
+                    Err(_) => {
                         "".to_owned()
                     },
                     Ok(f) => (f).to_string()
                 },
             };
 
-            // println!("rating: {}", rating);
             data.push(format!("\"{}\",{},\"{}\",{}",
                               title,
                               year,
                               directors.iter().format(","),
                               rating
             ));
-            // println!("{},{},\"{}\",{:.1}", title, year, directors.iter().format(","), rating)
         }
     }
 
-    std::thread::sleep(time::Duration::from_secs_f64(1.5));
-
-    let mut content = String::with_capacity(10000);
-    for d in data {
-        content.push_str(&d);
-        content.push('\n');
-    }
-
-    fs::write(
-        std::path::Path::new("result.csv"),
-        content
-    ).unwrap();
+    save_csv(data, &args.output_file);
 }
